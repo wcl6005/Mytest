e26b410aa17cbdb241e5689fb7fc638210a15e72
Wu Chunlong <wcl6005@126.com>
2018-02-20 21:19:04 +0800

    add index

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,6 @@ from .models import Poem
 
 # Create your views here.
 def home(request):
-    poems = Poem.objects.all()
-    return render(request, 'home.html', context=locals())
+    #poems = Poem.objects.all()
+    return render(request, 'index.html', context=locals())
 
