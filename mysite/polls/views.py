64e21aad20645bb1c47e383ee6370182fae5ba6a
Wu Chunlong <wcl6005@126.com>
2018-03-27 11:23:02 +0800

    del1

--- mysite/polls/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Poem
-
-# Create your views here.
-def home(request):
-    poems = Poem.objects.all()
-    return render(request, 'home.html', context=locals())
-
