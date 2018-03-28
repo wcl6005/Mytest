1e6d8fbc60e06024b414c994d3cc7c1f801d3903
Wu Chunlong <wcl6005@126.com>
2018-03-28 13:52:22 +0800

    aw

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,11 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView, ListView, View
-class IndexView(TemplateView):
-    template_name = 'home/index.html'
+# class IndexView(TemplateView):
+#     template_name = 'home/index.html'
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
-    url(r'^$', IndexView.as_view()),
+    #url(r'^$', IndexView.as_view()),
 ]
