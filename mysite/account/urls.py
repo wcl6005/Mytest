9f6b07d1d03278f559a8d74c707a4a3385c4ff26
Wu Chunlong <wcl6005@126.com>
2018-04-01 07:48:55 +0800

    ddd

--- mysite/account/urls.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import url, include
-
-from . import views
-
-urlpatterns = [
-    url(r'^billing/(?P<page>\d*)?$', views.billing, name='billing'),
-
-]
