323cb321f023c8bc8378df62cb761e10a312e095
Wu Chunlong <wcl6005@126.com>
2018-07-31 06:21:34 +0800

    add40

--- mysite/apisfun/users.py
+++ mysite/apisfun/users.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.contrib.auth.models import User, Group
+#from django.contrib.auth.models import User, Group
 from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login 
@@ -8,29 +8,11 @@ from django.contrib.auth import login as auth_login
  
 def registerapi(request):
     if request.method != 'POST':
-        mylist = list(User.objects.values())
+        mylist = [1,2]
         return  JsonResponse(mylist,safe=False)      
-    name = request.POST['name']
-    isname = User.objects.filter(username = name)
-    if isname:  
-        msgdict = {'msg': name + ' Username is already in name.'}
-        return JsonResponse(msgdict)      
-    password = request.POST['password']
-    email = request.POST['email']
-    user = User.objects.create_superuser(name, email, password)
-    user.save() 
-    return  JsonResponse({})
 
  
 def loginapi(request):   
     if request.method != 'POST':
-        mylist = list(User.objects.values())
+        mylist = [1,2]
         return  JsonResponse(mylist,safe=False)      
-    name = request.POST['name']
-    password = request.POST['password']
-    user = authenticate(username=name, password=password) 
-    if user:
-        auth_login(request, user)
-        return  JsonResponse({})      
-    msgdict = {'msg':'user authenticate err!'}
-    return JsonResponse(msgdict) 
