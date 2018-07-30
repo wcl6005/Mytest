3e525a398196624b2db4564ef5419a3eb3f3963f
Wu Chunlong <wcl6005@126.com>
2018-07-31 06:31:45 +0800

    add41

--- mysite/apisfun/users.py
+++ mysite/apisfun/users.py
@@ -4,15 +4,15 @@ from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login 
 from django.contrib.auth import login as auth_login 
-
+from django.http.response import HttpResponseRedirect,HttpResponse
  
 def registerapi(request):
     if request.method != 'POST':
         mylist = [1,2]
-        return  JsonResponse(mylist,safe=False)      
+        return  HttpResponse('ok')    
 
  
 def loginapi(request):   
     if request.method != 'POST':
         mylist = [1,2]
-        return  JsonResponse(mylist,safe=False)      
+        return  HttpResponse('ok')         
