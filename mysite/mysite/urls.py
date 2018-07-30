7a1c439f21d1b5b3dd845fa490c425ca048b4e32
Wu Chunlong <wcl6005@126.com>
2018-07-30 22:11:19 +0800

    add35

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -27,6 +27,5 @@ urlpatterns = [
     url(r'^account/', include('account.urls')),
     
     url(r'^$', RedirectView.as_view(url='/home/index/', query_string=True)),
-
     url(r'^home/', include('home.urls')),
 ]
