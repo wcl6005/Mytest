3cad1e2988895276ab869284c3e0057a11c6f715
Wu Chunlong <wcl6005@126.com>
2018-07-30 21:58:50 +0800

    add34

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
-#import home
+
 from django.views.generic import TemplateView, ListView, View
-#from myAPI import checkcode
+
 
 class IndexView(TemplateView):
     template_name = 'home/index.html'
 
 urlpatterns = [
     url(r'^index/$', IndexView.as_view()), 
-#     url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"), 
-#     url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"),        
-#     url(r'^myregister/', home.myregister, name="myregister"),    
-#     url(r'^mylogin/', home.mylogin, name="mylogin"),
 
 ]
