58d62ba53f6ada2409f209c0c8d5fc7f5775d6f1
Wu Chunlong <wcl6005@126.com>
2018-07-31 10:00:37 +0800

    add62

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,15 +2,11 @@
 from django.conf.urls import url, include
 from django.views.generic import TemplateView, ListView, View
 from myAPI import checkcode
-from . import home
+
 class IndexView(TemplateView):
     template_name = 'home/index.html'
 
 urlpatterns = [
     url(r'^index/$', IndexView.as_view()), 
-    url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"), # 
-    url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"), #        
-    url(r'^myregister/', home.myregister, name="myregister"), #   
-    url(r'^mylogin/', home.mylogin, name="mylogin"),
 
 ]
