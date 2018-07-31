9664897d9db0dee9e73e567b48a4c5ebfde8724e
Wu Chunlong <wcl6005@126.com>
2018-07-31 18:45:19 +0800

    add-api-login

--- mysite/home/apis/urls.py
+++ mysite/home/apis/urls.py
@@ -3,6 +3,8 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    url(r'^registerapi/', views.registerapi, name="registerapi"),
+    url(r'^loginapi/', views.loginapi, name="loginapi"),
     url(r'^index/', views.index, name="index"), 
 
 
