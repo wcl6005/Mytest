b3686652176e364d63a81daf25d54380940be3b4
Wu Chunlong <wcl6005@126.com>
2018-07-31 16:53:30 +0800

    add-myuser

--- mysite/apis/urls.py
+++ mysite/apis/urls.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
-import users
+import myuser
 
 urlpatterns = [
-    url(r'^registerapi/', users.registerapi, name="registerapi"),   
-    url(r'^loginapi/', users.loginapi, name="loginapi"),            
+    url(r'^registerapi/', myuser.registerapi, name="registerapi"),   
+    url(r'^loginapi/', myuser.loginapi, name="loginapi"),            
 ]
