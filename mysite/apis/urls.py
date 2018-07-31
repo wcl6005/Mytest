dee39ba123f93c383c8fb663bd6bdb76ec8660d2
Wu Chunlong <wcl6005@126.com>
2018-07-31 17:23:41 +0800

    del-apis

--- mysite/apis/urls.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf.urls import url, include
-import myuser
-
-urlpatterns = [
-    url(r'^registerapi/', myuser.registerapi, name="registerapi"),   
-    url(r'^loginapi/', myuser.loginapi, name="loginapi"),            
-]
