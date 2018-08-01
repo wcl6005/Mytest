e7735b730dffc963f904ec0d1a17478750d8070a
Wu Chunlong <wcl6005@126.com>
2018-08-01 17:06:47 +0800

    add-os

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,6 +2,17 @@
 from django.conf.urls import url, include
 from . import home
 
+import os
+from django.shortcuts import render
+try:
+    import cStringIO as StringIO
+except ImportError:
+    import StringIO
+import random
+from django.http.response import HttpResponseRedirect, HttpResponse
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+
 urlpatterns = [
     url(r'^test/', home.test, name="test"), 
     url(r'^registerapi/', home.registerapi, name="registerapi"), 
