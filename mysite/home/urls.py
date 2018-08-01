5ea52eb6ffa97d33abee54201f82b65bc9abfdc8
Wu Chunlong <wcl6005@126.com>
2018-08-01 16:59:21 +0800

    del-10

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 from . import home
-from myAPI import checkcode
+
 urlpatterns = [
     url(r'^test/', home.test, name="test"), 
     url(r'^registerapi/', home.registerapi, name="registerapi"), 
     url(r'^loginapi/', home.loginapi, name="loginapi"), 
 
-    url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"), # 
-    url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"), #        
+
 
 ]
