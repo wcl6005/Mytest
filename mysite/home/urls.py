579f999259d9c631c95b584885c50abe81b434fb
Wu Chunlong <wcl6005@126.com>
2018-08-01 09:05:58 +0800

    add-checkcode

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,9 +2,19 @@
 from django.conf.urls import url, include
 from . import home
 
+from django.views.generic import TemplateView, ListView, View
+from myAPI import checkcode
+
+ 
+# 本级路由：/home/
+class IndexView(TemplateView):
+    template_name = 'home/index.html'
+
 urlpatterns = [
     url(r'^test/', home.test, name="test"), 
     url(r'^registerapi/', home.registerapi, name="registerapi"), 
     url(r'^loginapi/', home.loginapi, name="loginapi"), 
+    url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"), # 
+    url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"), #        
 
 ]
