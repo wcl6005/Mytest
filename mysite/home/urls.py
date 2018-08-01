346dbc2d064aa8d65af2f7c3badc84b887a5f205
Wu Chunlong <wcl6005@126.com>
2018-08-01 19:25:47 +0800

    add-10

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -2,23 +2,23 @@
 from django.conf.urls import url, include
 from . import home
 
-# import os
-# from django.shortcuts import render
-# # try:
-# #     import cStringIO as StringIO
-# # except ImportError:
-# #     import StringIO
-# import random
-# from django.http.response import HttpResponseRedirect, HttpResponse
-#from PIL import Image, ImageDraw, ImageFont, ImageFilter
+from __future__ import unicode_literals
+import os,sys
+from io import BytesIO as StringIO
+from django.shortcuts import render
 
-from myAPI import checkcode
+import random
+from django.http.response import HttpResponseRedirect, HttpResponse
+from PIL import Image, ImageDraw, ImageFont, ImageFilter
+
+
+#from myAPI import checkcode
 urlpatterns = [
     url(r'^test/', home.test, name="test"), 
     url(r'^registerapi/', home.registerapi, name="registerapi"), 
     url(r'^loginapi/', home.loginapi, name="loginapi"), 
-    url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"),
-    url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"),
+#     url(r'^checkcodeGIF/', checkcode.checkcodeGIF, name="checkcodeGIF"),
+#     url(r'^getcheckcode/', checkcode.getcheckcode, name="getcheckcode"),
 
 
 ]
