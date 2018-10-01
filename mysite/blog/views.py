b18d9fe8fe247fd37268b395a081d3ccfed81d29
Wu Chunlong <wcl6005@126.com>
2018-10-01 21:37:26 +0800

    add2

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect, HttpResponse
-from myAPI.videoURL import video_url_list
+#from myAPI.videoURL import video_url_list
+
+video_url_list = ["https://www.jqaaa.com/jx.php?url=", #0
+        "https://5.5252e.com/jx/b.php?url=",
+        "http://yun.mt2t.com/yun?url=",        
+        "http://www.wmxz.wang/video.php?url=",    
+        "http://api.bbbbbb.me/yunjx/?url=",        
+        "http://000o.cc/jx/ty.php?url=",
+        "http://api.baiyug.cn/vip/index.php?url=", #6 le电视剧、爱奇艺
+        "http://yun.baiyug.cn/vip/?url=",
+];
 
 # Create your views here.
 #  http://localhost:9000/blog/videoplay/
