f6b3886825d8cb55ae01856d50664764818315a9
Wu Chunlong <wcl6005@126.com>
2018-10-08 08:52:38 +0800

    video_vip

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/wuchunlong/local/test/py3_videoplay/envpy3/Mytest/env/bin/python3.5
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
