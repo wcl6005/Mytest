4af8f38726b2f65499c6c3360aa8cebccc96f219
Wu Chunlong <wcl6005@126.com>
2019-01-14 18:08:05 +0800

    add

--- mysite/home/home.py
+++ mysite/home/home.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+#识别图像中的字符串 使用baidu-aip模块  优点是：快、准、简洁 
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect, HttpResponse
 from django.http import JsonResponse   
@@ -7,22 +8,16 @@ import os
 AppID = '11450107'
 API_Key = 'uAA5KGXAzDDk2ewBa3dvRrWj'
 Secret_Key = 'CgLyabrL4WH5KV0yyT074cMx8GyAnRGt'
-
-#使用baidu-aip模块 识别图像中的中文okokok! 优点是：快、准、简洁 
-#  http://localhost:8000/home/img_recognitions  
+IMG_NAME = 'img_name.jpg'
+ 
+#  http://localhost:8000  
 def img_recog(reuqest):  
-    data = {'imgdata': 'ok'}
+    data = {'imgdata': '识别图像中的字符串 okokok!'}
     return JsonResponse(data) 
 
-IMG_NAME = 'img_name.jpg'
-#  http://localhost:8000/distinguish_img/  
-def distinguish_img(request):
-    res = ''
-    client = AipOcr(AppID,API_Key,Secret_Key)
-    img = open(IMG_NAME,'rb').read()
-    msg = client.basicGeneral(img)
-    for m in msg.get('words_result'):
-        res += m.get('words') + '\n '
+#  http://localhost:8000/img_to_data/  
+def img_to_data(request):
+    res = get_img_str(IMG_NAME)
     mylist = [{"res" : res}]
     print(res) 
     return JsonResponse(mylist, safe = False) 
@@ -30,28 +25,36 @@ def distinguish_img(request):
 #  http://localhost:8000/wx_uploadFile/ 
 def wx_uploadFile(request):
     name = ''
-    if request.FILES:
-        MyImg = request.FILES.get("file", None)
-        if MyImg:
-            name = MyImg.name
-            if not WriteFile(MyImg,IMG_NAME): #保存图像文件 name_img.jpg
-                name = ''
-    mylist = [{"name" : name}] 
+    try:
+        if request.FILES:
+            MyImg = request.FILES.get("file", None)
+            if MyImg:
+                name = MyImg.name
+                WriteFile(MyImg, IMG_NAME) #保存图像文件 IMG_NAME
+    except Exception as ex:
+        print(ex)
+        name = ex 
+    mylist = [{"name" : name}]
     return JsonResponse(mylist, safe = False) 
+
+def get_img_str(name):
+    s = ''
+    client = AipOcr(AppID,API_Key,Secret_Key)
+    img = open(name,'rb').read()
+    msg = client.basicGeneral(img)
+    for m in msg.get('words_result'):
+        s += m.get('words') + '\n'
+    return s
+
 
 def WriteFile(fread,writefilepath):
     size = 1024 #每次读字节数
-    fp = open(writefilepath,'w+') #打开文件
-    try:
-        while True:
-            chunk = fread.read(size)
-            if not chunk:
-                break
-            fp.write(chunk)
-        fp.close()
-    except Exception as ex:
-        print(ex)
-        return False
-    return True
+    fp = open(writefilepath,'w+') 
+    while True:
+        chunk = fread.read(size)
+        if not chunk:
+            break
+        fp.write(chunk)       
+
    
 
--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url, include
 from . import home
 urlpatterns = [    
     url(r'^$', home.img_recog, name="img_recog"),    
-    url(r'^distinguish_img/', home.distinguish_img, name="distinguish_img"),     
+    url(r'^img_to_data/', home.img_to_data, name="img_to_data"),     
     url(r'^wx_uploadFile/', home.wx_uploadFile, name="wx_uploadFile"),     
 ]
