60ebc450246a5afa69f790d8eae067c209870350
Wu Chunlong <wcl6005@126.com>
2019-01-19 19:22:21 +0800

    add

--- mysite/home/home.py
+++ mysite/home/home.py
@@ -1,25 +1,47 @@
 # -*- coding: utf-8 -*-
 #识别图像中的字符串 使用baidu-aip模块  优点是：快、准、简洁 
+#baidu-aip模块 网站 https://console.bce.baidu.com/ai/?_=1530048735638&fromai=1#/ai/ocr/app/detail~appId=416472
+#图片转文字工具 http://www.gaitubao.com/tupian-wenzi/
 # 微信小程序在开发工具上执行POST请求这里(服务器)可以接受到数据；但是手机预览时执行POST请求这里(服务器)接受不到数据
 # 原因：域名信息未设置 https://blog.csdn.net/ycocol/article/details/79295504
-#headers = {"Content-Type": "application/x-www-form-urlencoded"}
 
 from django.shortcuts import render
 from django.http.response import HttpResponseRedirect, HttpResponse
 from django.http import JsonResponse   
 from aip import AipOcr   # pip install  baidu-aip
 import os
-from django.views.decorators.csrf import csrf_exempt
+from io import BytesIO as StringIO
+
+UPFILE_SIZE = 8000000 #设置上传文件大小(8MB)
+IMG_NAME = 'static_common/img_name.jpg'
+
 AppID = '11450107'
 API_Key = 'uAA5KGXAzDDk2ewBa3dvRrWj'
 Secret_Key = 'CgLyabrL4WH5KV0yyT074cMx8GyAnRGt'
-IMG_NAME = 'test_img.jpeg'
 
 #  http://localhost:8000  
 def index(request):
     return  render(request, 'index.html', context=locals())    
 
-#  http://localhost:8000  
+# 保存上传的文件
+def save_upfile(upfile):
+    ret, fp = False, open(IMG_NAME,'wb')  
+    if upfile.size < UPFILE_SIZE: #限制上传文件大小          
+        for chunk in upfile.chunks(): 
+            fp.write(chunk)       
+        ret = True
+    fp.close()
+    return ret
+
+def tostr(request):       
+    if request.method == 'POST':        
+        upfile = request.FILES.get("upfile", None)                              
+        res = get_distinguish_img_str(IMG_NAME) if save_upfile(upfile) \
+            else '上传文件大于 %s MB' %(UPFILE_SIZE/1000000)       
+        return  render(request, 'tostr.html', context=locals())      
+    return  render(request, 'recog.html', context=locals())
+
+#  http://localhost:8000/recog_img/  
 def recog_img(request):         
     res = get_distinguish_img_str(IMG_NAME)
     mylist = [{"res" : res}] 
@@ -37,26 +59,22 @@ def apidata(request):
     return JsonResponse(mylist, safe = False) 
 
 #  http://localhost:8000/distinguish_img/  
-def distinguish_img(request):
-    name ='name_img.jpg'   
-    res = get_distinguish_img_str(name)
+def distinguish_img(request):  
+    res = get_distinguish_img_str(IMG_NAME)
+    if os.path.exists(IMG_NAME):
+        os.remove(IMG_NAME)
     mylist = [{"res" : res}] 
     return JsonResponse(mylist, safe = False) 
 
- 
-
 #  http://localhost:8000/wx_uploadFile/  
-@csrf_exempt
 def wx_uploadFile(request):
     name = '' 
     if request.method == 'POST':   
-        myfile = request.FILES.get("file", None)                              
-        if myfile:
-            WriteFile(myfile)
-            name = myfile.name 
-        else:    
-            if os.path.exists('name_img.jpg'):
-                os.remove('name_img.jpg')
+        upfile = request.FILES.get("upfile", None)                              
+        if upfile:
+            name = myfile.name if save_upfile(upfile) \
+            else '上传文件大于 %s MHZ' %(UPFILE_SIZE/1000000)                    
+        else:                 
             name = 'None'         
     mylist = [{"name" : name}] 
     return JsonResponse(mylist, safe = False) 
@@ -74,14 +92,5 @@ def get_distinguish_img_str(name):
     if not s:
         s = 'No Img Data !'
     return s
- 
-def WriteFile(myfile):
-    try:
-        with open('name_img.jpg','wb') as file:
-            file.write(myfile.read())
-    except Exception as ex:
-        with open('name_img.jpg','wb') as file:
-            file.write(str(ex).encode("utf-8"))
-
-   
+    
 
--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,7 +3,9 @@ from django.conf.urls import url, include
 from . import home
 urlpatterns = [    
     url(r'^$', home.index, name="index"),
+    url(r'^tostr/', home.tostr, name="tostr"),     
     url(r'^recog_img/', home.recog_img, name="recog_img"),    
+  
     url(r'^apidict/', home.apidict, name="apidict"),  
     url(r'^apidata/', home.apidata, name="apidata"),     
     url(r'^distinguish_img/', home.distinguish_img, name="distinguish_img"),     
