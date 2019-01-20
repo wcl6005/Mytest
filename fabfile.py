ec1a6d1d4400b2913a79fc26bc7a6a1d3335995d
Wu Chunlong <wcl6005@126.com>
2019-01-20 11:03:51 +0800

    add

--- fabfile.py
+++ fabfile.py
@@ -5,13 +5,14 @@ import datetime
 from fabric.api import (cd, env, lcd, put, prompt, local, sudo, run,
                         prefix, shell_env, settings, hide)
 
+git_db_passwd = '6yhn(IJN&U*'
 
-# fab -c fabricrc git_push
+# fab git_push
 def git_push():
-    local('git add . && git commit -a -m "add" && git push')
+    local('rm -rf env && git add . && git commit -a -m "add" && git push')
     
-# fab -c fabricrc push_git
-def push_git(): 
+# fab push
+def push(): 
     '''
     1、创建 db.txt
     2、push 工程
@@ -21,7 +22,7 @@ def push_git():
         'chmod -R 777 db1 && '
         'cp ./mysite/db.sqlite3 ./db1/production.sqlite3 && '
         'tar -zcvf - db1|openssl des3 -salt -k "%s" | dd of=db.txt && '
-        'rm -rf db1 || [ false ]' %(env.git_db_passwd))  
+        'rm -rf db1 || [ false ]' %(git_db_passwd))  
     git_push()
 
 
