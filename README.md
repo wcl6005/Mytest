a4dc2518c8f63186f34cad848675289596f63f0f
Wu Chunlong <wcl6005@126.com>
2019-01-29 18:11:18 +0800

    add

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 3、django程序运行，前台有动画的图标
 4、前台实现文件内容的拷贝、粘贴。
 5、后台实现上传文件大小设定：支持识别的文件大小,不得大于8MB。
+6、在线生成条形码、二维码。
+
+五、存在问题
+1、批量生成条形码，输入框第二行内容也预览了。正确：应该只预览第一行内容。
+2、QR_code.html中，qrcode16.js文件，在线使用和下载到本地使用，对浏览器Firefox支持有影响
+<script src="https://static.gaitubao.net/js/qrcode16.js"></script> 不支持浏览器：Firefox 
+<script src="/static/prettify/js/qrcode16.js"></script>   不知为什么？
--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
 from . import home
-urlpatterns = [    
+urlpatterns = [ 
+    url(r'^test/', home.test, name="test"), 
+        
     url(r'^$', home.index, name="index"),
     url(r'^tostr/', home.tostr, name="tostr"),     
     url(r'^recog_img/', home.recog_img, name="recog_img"),    
@@ -11,4 +13,9 @@ urlpatterns = [
     url(r'^distinguish_img/', home.distinguish_img, name="distinguish_img"),     
     url(r'^wx_uploadFile/', home.wx_uploadFile, name="wx_uploadFile"),     
 
+    url(r'^bar_code/', home.bar_code, name="bar_code"),
+    url(r'^batch_code/', home.batch_code, name="batch_code"),
+    
+    url(r'^QR_code/', home.QR_code, name="QR_code"), 
+    
 ]
